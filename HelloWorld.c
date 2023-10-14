bea1645c29aaa4fe69039d4ac6f3cf2535a9553f
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-14 15:28:57 -0700

    variables with print statements

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,7 +2,20 @@
 
 int main()
 {
+    // basic print statements
     printf("hello from C\n");
     printf("C is cool!\n");
+
+    // variables
+    int x = 123;
+    float y = 1.23;
+    char z = 'a';
+    char a[] = "hello world";
+
+    printf("%i\n", x);
+    printf("%f\n", y);
+    printf("%c\n", z);
+    printf("%s", a);
+
     return 0;
 }
